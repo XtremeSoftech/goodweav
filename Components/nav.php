bc4fa5ca08d7579655b178ba3c43ade619dc4e5f
joelcharles <joelcharlespati@gmail.com>
2021-07-08 16:25:48 +0530

    updated few tables in UI

--- Components/nav.php
+++ Components/nav.php
@@ -8,10 +8,10 @@
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <!-- <a class="nav-link" href="#">Features</a> -->
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="labelsrequested.php">Requested LABELS</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
