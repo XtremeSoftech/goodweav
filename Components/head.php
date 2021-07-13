e04a5072750b1ad837acedd0e091865daf78b9b6
JOEL CHARLES <66256907+joelcharles@users.noreply.github.com>
2021-07-13 21:20:28 +0530

    new Exporter UI

--- Components/head.php
+++ Components/head.php
@@ -4,8 +4,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./assets/css/style.css">
+
+    <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    
     
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
 
--- addexp.php
+++ addexp.php
@@ -11,15 +11,52 @@
 
     <section id="expimp">
         <div class="container">
-            <div class="row">
-                <div class="col-md-3 expimp-left">
-                    <i class="fas fa-file-export"></i>
-                    <h3>Add Exporter</h3>
-                    <p>Exporter details to be entered as in GWI records</p>
-                </div>
-                <div class="col-md-9 expimp-right">
-                    
+            <div class="col-lg-12 col-sm-12">
+                <div class="row">
+                    <div class="col-md-3 col-sm-12 expimp-left">
+                        <i class="fas fa-file-export"></i>
+                        <h3>Add Exporter</h3>
+                        <p>Exporter details to be entered as in GWI records</p>
+                    </div>
+                    <div class="col-md-9 col-sm-12 expimp-right">
+                        <h4>Enter NEW Exporter Details</h4>
+                        <div class="fields">
+                            <form action="addnewexporter">
+                                
+                                    <div class="form-group">
+                                        <input class="col-md-3 col-sm-12" type="text" placeholder="Exporter Code" required>
+                                        <input class="col-md-3 col-sm-12" type="date" placeholder="Registration Date" required>
+                                        <input class="col-md-5 col-sm-12" type="email" placeholder="Exporter Email" required>
+                                    </div>
+                                
+                                
+                                    <div class="form-group">
+                                        <input class="col-md-7 col-sm-12" type="text" placeholder="Exporter Name" required>
+                                        <input class="col-md-4 col-sm-12" type="text" placeholder="Contact No. with Country Code" required>
+                                    </div>
+                                
+                                
+                                    <div class="form-group">
+                                        <input class="col-md-11" type="text" placeholder="Exporter Address" required>
+                                    </div>
 
+                                    <div class="form-group">
+                                        <input class="col-md-4" type="text" placeholder="Exporter Country" required>
+                                        <input class="col-md-4" type="url" placeholder="Exporter Website URL" required>
+                                        <input class="col-md-3" type="text" placeholder="Exporter GSTIN" required>
+                                    </div>
+
+                                    <!-- <hr> -->
+
+                                    <div class="form-group">
+                                        <input class="col-md-7 col-sm-12" type="text" placeholder="Exporter License Name" required>
+                                        <input class="col-md-4 col-sm-12" type="text" placeholder="Exporter License Number" required>
+                                    </div>
+                                
+                            </form>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
